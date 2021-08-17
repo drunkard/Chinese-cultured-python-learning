f8564e28cb8ed6fded833b5a41d00999ecbba45e
Drunkard Zhang <gongfan193@gmail.com>
2021-08-17 14:41:40 +0800

    while optimize
    Signed-off-by: Drunkard Zhang <gongfan193@gmail.com>

--- programs/while.py
+++ programs/while.py
@@ -4,15 +4,15 @@ import time
 def eat_loop():
     cs = os.get_terminal_size().columns
     counter = 0
+    s = ' 猫 吃 鼠'
+    slen = 9  # s 的长度
     while True:
-        s = ' 猫 吃 鼠'
-        slen = 9  # s 的长度
         if counter <= (cs-slen):
             print('\b' * (slen - 1) + s, end='', flush=True)
         else:
             back = '\b' * (slen + 1)
             print(back + s + '\b', end='', flush=True)
-        if counter >= (cs-slen)*2:
+        if counter >= (cs-slen*2)*2:
             counter = 0
         counter += 1
         time.sleep(0.1)
@@ -46,6 +46,6 @@ def tj2(to=100):
 
 
 if __name__ == '__main__':
-    # eat_loop()
+    eat_loop()
     # sum_numbers(100)
-    tj2()
+    # tj2()
